9e2d8a04f5834f2d624c28d55b974a89747d7a62
Wang Xinyu <hi_wangxinyu@163.com>
2024-03-04 17:22:21 +0800

    优化文档

--- controller/src/schedule/get_routing.py
+++ controller/src/schedule/get_routing.py
@@ -9,11 +9,14 @@ from sqlmodel import Session,select
 from typing import Sequence
 from ..orm import engine,Connection,Business
 
-def greedy(connections:Sequence[Connection],businesses:Sequence[Business]) -> dict[int, list[str]]:
-    """使用贪婪算法计算符合业务要求的路由"""
+# def greedy(connections:Sequence[Connection],businesses:Sequence[Business]) -> dict[int, list[str]]:
+#     """使用贪婪算法计算符合业务要求的路由"""
+#     return {1:['0000:0000:0000:0000']}
+
+def static(connections:Sequence[Connection],businesses:Sequence[Business]) -> dict[int, list[str]]:
     return {1:['0000:0000:0000:0000']}
 
-route_algorithm=greedy
+route_algorithm=static
 
 with Session(engine) as session:
     statement = select(Business)

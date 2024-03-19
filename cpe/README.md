b920209a1d9a4d705e0266e8518f9c671d280c1d
Wang Xinyu <hi_wangxinyu@163.com>
2024-03-20 01:12:27 +0800

    删除p4代码中用不到的部分，添加脚本

---
+++ cpe/README.md
@@ -0,0 +1,11 @@
+## 运行流程
+1. install.sh 疑似断电重启后，需要运行一次，配置某个系统设置
+2. compile.sh 修改代码后，重新编译p4代码
+3. run.sh 启动p4程序
+    1. 会调用shutdown.sh 杀死现有的p4
+4. run_setup.sh 添加流表
+5. open_shell.sh 连接到bft cli
+
+## 使用程序添加流表的方案
+1. 由于缺少文档，尝试通过追溯run_setup.sh找到添加流表的其他方案。但最终发现添加流表通过bfshell，这是一个二进制文件而非脚本。
+1. 目前唯一已知的添加流表方式是合成python文件，然后通过/root/bf-sde-9.1.0/run_bfshell.sh -b xxx.py添加。
--- cpe/compile.sh
+++ cpe/compile.sh
@@ -1,15 +1,14 @@
-#TODO
 ##编译（初始环境编译）：
 export SDE=/root/bf-sde-9.1.0
 export SDE_INSTALL=$SDE/install
 export PATH=$SDE_INSTALL/bin:$PATH
 
-#加载bf驱动
-#$SDE_INSTALL/bin/bf_kdrv_mod_load $SDE_INSTALL
+#加载bf驱动（断电重启后需要重新加载）
+#$SDE_INSTALL/bin/bf_kdrv_mod_load $SDE_INSTALL 
+
 cd $SDE/pkgsrc/p4-build
 ./autogen.sh
 
-#程序编译
 $SDE/pkgsrc/p4-build/configure --with-tofino --with-p4c=p4c --prefix=$SDE_INSTALL \
 --bindir=$SDE_INSTALL/bin \
 P4_NAME=srv6 \
@@ -20,5 +19,6 @@ LDFLAGS="-L$SDE_INSTALL/lib" \
 
 make
 make install
+
 
 #/root/bf-sde-9.1.0/run_switchd.sh -p srv6

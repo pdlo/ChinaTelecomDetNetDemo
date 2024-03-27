f37119816a4053559a9b399b0a0e70a92220402b
jiumingaaa <10833710+jiumingaaa@user.noreply.gitee.com>
2024-03-27 11:42:19 +0800

    更改启动脚本，使得运行时可以直接开启端口，不用进入bfshell中输入

--- cpe/run_p4.sh
+++ cpe/run_p4.sh
@@ -6,5 +6,9 @@ export SDE=/root/bf-sde-9.1.0
 export SDE_INSTALL=$SDE/install
 export PATH=$SDE_INSTALL/bin:$PATH
 
+#这里153可以换成任意会话名
+screen -S 153 -dm bash -c '/root/bf-sde-9.1.0/run_switchd.sh -p srv6 && exit'
+#要用command.txt文件
+/root/bf-sde-9.1.0/run_bfshell.sh -f /root/my_p4/srv6/command.txt
 
-/root/bf-sde-9.1.0/run_switchd.sh -p srv6
+#/root/bf-sde-9.1.0/run_switchd.sh -p srv6

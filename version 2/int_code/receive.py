279e2632e17aa78aa4cfbdc90b1bd799ac266fbe
msy12134 <130446495+msy12134@users.noreply.github.com>
2024-05-25 11:37:15 +0800

    Create receive.py

---
+++ version 2/int_code/receive.py
@@ -0,0 +1,26 @@
+from scapy.all import *
+from scapy.layers.inet import UDP
+from header_definition import *
+def get_if():
+    ifs=get_if_list()
+    iface=None
+    for i in ifs:
+        if "eth0" in i:
+            iface=i
+            break
+    if iface==None:
+        print("cannot find the interface")
+        exit(1)
+    return iface
+iface=get_if()
+def packet_handler(pkt):
+    while True:
+    	pkt.show2()
+
+
+def receive_packets():
+    print(f"start receiving on {iface}")
+    sniff(iface=iface,prn=packet_handler)
+
+
+receive_packets()

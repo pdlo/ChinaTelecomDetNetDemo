daee0a66086649da347ed4cbadc407c2b0f25a9d
msy12134 <130446495+msy12134@users.noreply.github.com>
2024-05-26 17:45:43 +0800

    Update send.py

--- version 2/int_code/send.py
+++ version 2/int_code/send.py
@@ -1,3 +1,4 @@
+
 import time
 from scapy.arch import get_if_hwaddr
 from scapy.interfaces import get_if_list
@@ -20,57 +21,58 @@ def get_if():
         exit(1)
     return iface
 iface=get_if()
+
 def send_ipv4_icmp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IP(src='10.0.0.1',dst='10.0.0.2')/ICMP())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv4_udp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IP(src='10.0.0.1',dst='10.0.0.2')/UDP())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv4_tcp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IP(src='127.0.0.1',dst='127.0.0.1')/TCP())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv6_icmp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IPv6(src='::100',dst='::200')/ICMPv6EchoRequest())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv6_udp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IPv6(src='::100',dst='::200')/UDP())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv6_tcp():
-    packet=(Ether(src=get_if_hwaddr(iface))/
+    packet=(Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/
             IPv6(src='::100',dst='::200')/TCP())
     while True:
         packet.show()
         sendp(packet,iface=iface)
         time.sleep(1)
 def send_ipv4_INT():
-    packet=Ether(src=get_if_hwaddr(iface))/IP(src='127.0.0.1',dst='127.0.0.1',tos=1)/ \
+    packet=Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff")/IP(src='127.0.0.1',dst='127.0.0.1',tos=1)/ \
             probe_t(data_cnt=0)
     while True:
         packet.show()
         sendp(packet, iface=iface)
         time.sleep(1)
 def send_ipv4_INT_INTDATA():#这个包是为了检测接收端能不能正确解析携带数据的INT包，发送端不会发送在这个包
-    packet = Ether(src=get_if_hwaddr(iface)) / IP(src='127.0.0.1', dst='127.0.0.1', tos=1) / \
+    packet = Ether(src=get_if_hwaddr(iface),dst="ff:ff:ff:ff:ff:ff") / IP(src='127.0.0.1', dst='127.0.0.1', tos=1) / \
              probe_t(data_cnt=0)/probe_data_h(port_ingress=1,port_egress=2,current_time_ingress=10,packet_cnt_ingress=15,
                                               packet_cnt_egress=20,packet_len_ingress=100,packet_len_egress=200)/ \
              probe_data_h(port_ingress=1, port_egress=2, current_time_ingress=10, packet_cnt_ingress=15,
@@ -79,4 +81,4 @@ def send_ipv4_INT_INTDATA():#这个包是为了检测接收端能不能正确解
         packet.show()
         sendp(packet, iface=iface)
         time.sleep(1)
-send_ipv4_INT_INTDATA()   #具体发哪个包
+send_ipv4_INT()   #具体发哪个包
